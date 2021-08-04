36c0428c3224a7d698828897a7f26d0408a181ba
Aluno <Aluno@LAB2-12>
2021-08-04 15:56:01 -0300

    Adicionar arquivos de projeto.

---
+++ atividade1/Produto.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace atividade1
+{
+    class Produto
+    {
+        private string Nome { get; set; }
+        private double Preco { get; set; }
+        private int Quantidade { get; set; }
+
+        public Produto(string nome, double preco, int quantidade)
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+        public void AdicionarProduto(int qtd)
+        {
+            Quantidade += qtd;
+        }
+        public void RemoverProduto(int qtd)
+        {
+            Quantidade -= qtd;
+        }
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+        public override string ToString()
+        {
+            return $"Dados do produto: {Nome}, $ {Preco}, {Quantidade} unidades, Total: $ {ValorTotalEmEstoque()}";
+        }
+    }
+}
---
+++ atividade1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace atividade1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Entre os dados do produto:\nNome: ");
+            string nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            double preco = double.Parse(Console.ReadLine());
+            Console.Write("Quantidade: ");
+            int quantidade = int.Parse(Console.ReadLine());
+
+            Produto Prod1 = new Produto(nome, preco, quantidade);
+
+            Console.WriteLine(Prod1.ToString());
+            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+            int quant = int.Parse(Console.ReadLine());
+            Prod1.AdicionarProduto(quant);
+            Console.WriteLine(Prod1.ToString());
+            Console.Write("Digite o número de produtos a ser removido do estoque: ");
+            quant = int.Parse(Console.ReadLine());
+            Prod1.RemoverProduto(quant);
+            Console.WriteLine(Prod1.ToString());
+        }
+    }
+}
